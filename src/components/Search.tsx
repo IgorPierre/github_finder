9e32d7d2e506bd6892c5772374353b15370780e4
Igor Pierre <igor.pierre45@gmail.com>
2023-02-08 10:20:12 -0300

    create the API connection

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,30 @@
+type SearchProps = {
+    loadUser:(userName: string) => Promise<void>;
+}
+
+import { useState } from 'react'
+
 import { BsSearch } from 'react-icons/bs'
 
-function Search() {
+function Search({loadUser}: SearchProps) {
+
+    const [userName, setUserName] = useState("");
+
     return(
         <div>
             <h2>Buscar usuário</h2>
 
-            <form>
-                <input type="text" placeholder='Digite o nome do usuário' />
-                <button><BsSearch/></button>
-            </form>
+            <div>
+                <input 
+                    type="text"
+                    placeholder='Digite o nome do usuário'
+                    onChange={(e) => setUserName(e.target.value)} 
+                />
+
+                <button onClick={() => loadUser(userName)}>
+                    <BsSearch/>
+                </button>
+            </div>
         </div>
     )
 }
