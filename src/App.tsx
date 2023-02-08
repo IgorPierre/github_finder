585b09824ccfedea607926d7c54a0a9d136cfff8
Igor Pierre <igor.pierre45@gmail.com>
2023-02-08 09:35:35 -0300

    create the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Outlet } from "react-router-dom"
+
 function App() {
  
   return (
     <div className="App">
-      <h1>olá mundo</h1>
+      <Outlet/>
     </div>
   )
 }
---
+++ src/routes/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return(
+        <div>
+            <h1>olá, sou a home !</h1>
+        </div>
+    )
+}
+
+export default Home;
